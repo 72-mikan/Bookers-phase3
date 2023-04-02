7380253377f74c2e1751046cb5777d281f323678
72_mikan_desktop <shinzi7280.18@gmail.com>
2023-04-02 14:59:59 +0900

    kadai-9c ボタンの実装

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -8,9 +8,7 @@ class GroupUsersController < ApplicationController
   end
 
   def destroy
-    puts 'test1'
     if GroupUser.exists?(group_id: params[:group_id], user_id: current_user.id)
-      puts 'test2'
       group_user = current_user.group_users.find_by(group_id: params[:group_id])
       group_user.destroy
       redirect_to groups_path
